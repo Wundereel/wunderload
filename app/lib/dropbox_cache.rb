ec3be66f5781f1c311462cc397c6ff084b6cf37a
Ryan Sorensen <rcs@daedalus.local>
2015-05-30 14:03:57 -0700

    redis cloud config

--- app/lib/dropbox_cache.rb
+++ app/lib/dropbox_cache.rb
@@ -5,15 +5,7 @@ class DropboxCache
   include Singleton
 
   def client
-    return @client if @client
-    if ENV['REDISCLOUD_URL']
-      uri = URI.parse(ENV["REDISCLOUD_URL"])
-      redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-    else
-      redis = Redis.new
-    end
-
-    @client = Redis::Namespace.new(:dropbox_cache, redis: redis)
+    @client ||= Redis::Namespace.new(:dropbox_cache)
   end
 
   def delta_for(token, cursor)
