3cceb94f35cf374b4c81f1de9faf0598fa0693a0
actonric <richard@wundereel.com>
2016-03-14 00:05:48 -0700

    undoing local/dev port commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_controller.asset_host = 'http://localhost:3000'
+  config.action_controller.asset_host = 'http://localhost:5000'
   config.action_mailer.asset_host = config.action_controller.asset_host
 
   # Print deprecation notices to the Rails logger.
