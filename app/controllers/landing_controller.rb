82a1600044a7313bb1265e2159aa546669c67176
Ryan Sorensen <rcsorensen@gmail.com>
2016-05-16 17:50:33 -0700

    embedded typeform

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,7 +1,16 @@
 class LandingController < ApplicationController
   include HighVoltage::StaticPage
 
-  layout 'bare'
+  layout :layout_for_page
+
+  def layout_for_page
+    case params[:id]
+    when 'questions'
+      false
+    else
+      'bare'
+    end
+  end
 
   def page_finder_factory
     HighVoltage::PageFinder
