a947949511456f80082ac593ba6d10b43869bd38
Ryan Sorensen <rcsorensen@gmail.com>
2016-06-24 00:09:02 -0700

    add sabayon configger

--- config/routes.rb
+++ config/routes.rb
@@ -47,4 +47,14 @@ Rails.application.routes.draw do
       resources :user_files, only: [:index]
     end
   end
+
+  if ENV['ACME_KEY'] && ENV['ACME_TOKEN']
+    get ".well-known/acme-challenge/#{ ENV["ACME_TOKEN"] }" => proc { [200, {}, [ ENV["ACME_KEY"] ] ] }
+  else
+    ENV.each do |var, _|
+      next unless var.start_with?("ACME_TOKEN_")
+      number = var.sub(/ACME_TOKEN_/, '')
+      get ".well-known/acme-challenge/#{ ENV["ACME_TOKEN_#{number}"] }" => proc { [200, {}, [ ENV["ACME_KEY_#{number}"] ] ] }
+    end
+  end
 end
